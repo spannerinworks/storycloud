ca8e9d83e4ded8ab1cc350ac4aba120e8829894a
Ben Wong <ben.wong@unboxedconsulting.com>
2012-06-11 17:06:39 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storycloud::Application.config.secret_token = 'c970a94730f92c68785d06568523be890c19e90c19165745a9a3c0a7b4f25fead4e4bc85d1dd3a5f5f3c8232bd887035ddb797067787af5e1a0f304b36556ea6'
