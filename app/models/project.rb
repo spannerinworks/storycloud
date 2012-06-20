d8d0ec5ad6052da51f77f3648efa0666d5ec9d62
Ben Wong <ben.wong@unboxedconsulting.com>
2012-06-20 18:23:24 +0100

    add version validation to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,15 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :version
 
   has_many :cards
 
   validates_presence_of :name
+  validates_uniqueness_of :name
+
+  validate :version_must_be_incremented, :on => :update
+
+  protected
+  def version_must_be_incremented
+    errors.add(:version, 'version must be incremented') if version - version_was != 1
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,8 +6,26 @@ describe Project do
     Project.new(:name => 'alonso').save.should be_true
   end
 
+  it "should require a unique name" do
+    Project.create(:name => 'alonso')
+    Project.new(:name => 'alonso').valid?.should be_false
+  end
+
+  it "should have a default version of 0" do
+    Project.create(:name => 'alonso').version.should == 0
+  end
+
+  it "should reject an update if version isn't incremented'" do
+    p = Project.create(:name => 'alonso', :version => 17)
+
+    p.update_attributes(:name => 'ronaldo', :version => 17).should be_false
+    p.update_attributes(:name => 'ronaldo', :version => 19).should be_false
+
+    p.update_attributes(:name => 'ronaldo', :version => 18).should be_true
+  end
+
   it "should have many cards" do
-    project = create(:project)
+    project = build(:project)
     card1 = build(:card)
     card2 = build(:card)
     project.cards << card1
@@ -18,6 +36,5 @@ describe Project do
     project.reload
 
     project.cards.should == [card1, card2]
-
   end
 end
